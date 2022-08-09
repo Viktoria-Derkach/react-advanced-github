5057664d8b868a8a9765c9a085874446f215271f
ViktoriiaDerkach <viktoriia.derkach@airslate.com>
2022-08-09 22:01:48 +0300

    hook, rtc

---
+++ src/hooks/debounce.ts
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react"
+
+export const useDebounce = (value : string, delay = 300 ): string => {
+  const [debounced, setDebounced] = useState(value)
+  useEffect(() => {
+   const handler = setTimeout(() => setDebounced(value), delay);
+  return () => clearTimeout(handler)
+  }, [delay, value])
+return debounced
+}
